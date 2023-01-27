a7cdce3113ceb15e586ad4f5be09b1d1ed3566b8
byokurt <osman.kurt@msn.com>
2023-01-27 21:47:33 +0300

    editedt

--- Proxies/Demo/DemoApiProxy.cs
+++ Proxies/Demo/DemoApiProxy.cs
@@ -1,4 +1,8 @@
-﻿using ProblemDetailsExample.Extensions;
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using ProblemDetailsExample.Extensions;
 using ProblemDetailsExample.Proxies.Demo.Requestes;
 using ProblemDetailsExample.Proxies.Demo.Responses;
 
@@ -25,13 +29,39 @@ public class DemoApiProxy : IDemoApiProxy
         {
             string responseContent = await httpResponseMessage.Content.ReadAsStringAsync();
 
-            _logger.LogError("There was a problem while getting data from Demo Api. ResponseStatusCode:{ResponseStatusCode} ResponseBody:{ResponseBody}", (int) httpResponseMessage.StatusCode, responseContent);
+            _logger.LogError("There was a problem while getting data from Demo Api. ResponseStatusCode:{ResponseStatusCode} ResponseBody:{ResponseBody}", (int)httpResponseMessage.StatusCode, responseContent);
 
-            throw new ApplicationException($"There was a problem while getting data from Demo Api ResponseStatusCode:{(int) httpResponseMessage.StatusCode} ResponseBody:{responseContent}");
+            throw new ApplicationException($"There was a problem while getting data from Demo Api ResponseStatusCode:{(int)httpResponseMessage.StatusCode} ResponseBody:{responseContent}");
         }
 
         DemoResponse? demoResponse = await httpResponseMessage.Content.ReadAsJsonAsync<DemoResponse>();
 
         return demoResponse;
+    }
+
+    public async Task UpdateUser(int id, UpdateDemoRequest request)
+    {
+        JsonPatchDocument document = new JsonPatchDocument();
+
+        string requestModel = JsonSerializer.Serialize(document.Operations);
+
+        StringContent httpContent = new StringContent(requestModel, Encoding.UTF8, "application/json-patch+json");
+
+        HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Patch, $"v1/demo/{id}")
+        {
+            Content = httpContent
+        };
+
+        HttpResponseMessage responseMessage = await _httpClient.SendAsync(requestMessage);
+
+        if (!responseMessage.IsSuccessStatusCode)
+        {
+            ProblemDetails? problemDetails = await responseMessage.Content.ReadFromJsonAsync<ProblemDetails>();
+
+            if (problemDetails != null)
+            {
+                _logger.LogError($"Patch api call failed. Status Code: {problemDetails.Status}");
+            }
+        }
     }
 }
---
+++ Proxies/Demo/Requestes/UpdateDemoRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProblemDetailsExample.Proxies.Demo.Requestes;
+
+public class UpdateDemoRequest
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+}
