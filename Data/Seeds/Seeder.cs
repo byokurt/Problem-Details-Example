957cf7f9711eb4a88de3c39459c709ec2fce0ee3
Osman KURT <osmankurt@Osman-MacBook-Pro.local>
2023-03-18 19:17:08 +0300

    added data seed

---
+++ Data/Seeds/Seeder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ProblemDetailsExample.Data.Entities;
+
+namespace ProblemDetailsExample.Data.Seeds;
+
+public static class Seeder
+{
+    public static async Task MigrateWithData(this IHost host)
+    {
+        using IServiceScope scope = host.Services.CreateScope();
+
+        await using DemoDbContext demoDbContext = scope.ServiceProvider.GetRequiredService<DemoDbContext>();
+
+        await demoDbContext.Database.MigrateAsync();
+
+        User user = new User()
+        {
+             Name = "Osman",
+             Surename = "KURT",
+             IsDeleted = false           
+        };
+
+        demoDbContext.Users.Add(user);
+
+        await demoDbContext.SaveChangesAsync();
+    }
+
+}
+
